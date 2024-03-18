8f11d3a128f24741225d3123c5458512c67c04be
YUNUS GÜN <yunusgun@selamsahips-MacBook-Pro.local>
2024-03-18 17:10:52 +0300

    add observability library

--- src/main/java/demo/app/JavaDemoapp.java
+++ src/main/java/demo/app/JavaDemoapp.java
@@ -2,6 +2,10 @@ package demo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.pyroscope.javaagent.PyroscopeAgent;
+import io.pyroscope.javaagent.config.Config;
+import io.pyroscope.javaagent.EventType;
+import io.pyroscope.http.Format;
 
 @SpringBootApplication
 public class JavaDemoapp {
@@ -9,4 +13,16 @@ public class JavaDemoapp {
   public static void main(String[] args) {
     SpringApplication.run(JavaDemoapp.class, args);
   }
+}
+@PostConstruct
+public void init() {
+
+    PyroscopeAgent.start(
+    new Config.Builder()
+        .setApplicationName("yunus-java-demo-app")
+        .setProfilingEvent(EventType.ITIMER)
+        .setFormat(Format.JFR)
+        .setServerAddress("http://35.221.186.93:30002")
+        .build()
+    );
 }
