4093f9e2e6d254f3f778fa3a4be7d23512785ba0
yunusgun <99205332+yunusgun@users.noreply.github.com>
2024-03-19 14:02:13 +0300

    Update JavaDemoapp.java

--- src/main/java/demo/app/JavaDemoapp.java
+++ src/main/java/demo/app/JavaDemoapp.java
@@ -2,6 +2,8 @@ package demo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import javax.annotation.PostConstruct;
 import io.pyroscope.javaagent.PyroscopeAgent;
 import io.pyroscope.javaagent.config.Config;
 import io.pyroscope.javaagent.EventType;
@@ -10,19 +12,19 @@ import io.pyroscope.http.Format;
 @SpringBootApplication
 public class JavaDemoapp {
 
-  public static void main(String[] args) {
-    SpringApplication.run(JavaDemoapp.class, args);
-  }
-}
-@PostConstruct
-public void init() {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDemoapp.class, args);
+    }
 
-    PyroscopeAgent.start(
-    new Config.Builder()
-        .setApplicationName("yunus-java-demo-app")
-        .setProfilingEvent(EventType.ITIMER)
-        .setFormat(Format.JFR)
-        .setServerAddress("http://35.221.186.93:30002")
-        .build()
-    );
+    @PostConstruct
+    public void init() {
+        PyroscopeAgent.start(
+            new Config.Builder()
+                .setApplicationName("yunus-java-demo-app")
+                .setProfilingEvent(EventType.ITIMER)
+                .setFormat(Format.JFR)
+                .setServerAddress("http://35.221.186.93:30002")
+                .build()
+        );
+    }
 }
